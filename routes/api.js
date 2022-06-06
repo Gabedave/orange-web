267720780d9afa66fb3c5b14eb17d63e97331095
David Gabriel <gabedave01@gmail.com>
2022-06-06 08:12:59 +0100

    add automatic download script

--- routes/api.js
+++ routes/api.js
@@ -6,8 +6,8 @@ const stream = defaultExport
 // Perform Database Search
 async function searchDB(model, mediaId) {
   if (!mediaId) return
-  const mod = await model.findOne({"mediaId":mediaId});
-  
+  const mod = await model.findOne({ "igMediaId": mediaId });
+
   async function callBack(doc) {
 
     if (!doc) {
@@ -20,47 +20,47 @@ async function searchDB(model, mediaId) {
   return await callBack(mod);
 }
 
-export default function (app,model) {
+export default function (app, model) {
 
   app.route('/igmedia/:mediaId')
-    .get(async function (req, res, next){
+    .get(async function (req, res, next) {
       let mediaId = req.params.mediaId;
-      
+
       if (!mediaId) res.status(404)
 
-      res.render('mediapage', {mediaId: mediaId, mediaUrl: `/api/stream/?${mediaId}`});
+      res.render('mediapage', { mediaId: mediaId, mediaUrl: `/api/stream/?${mediaId}` });
       // SET TO RETURN THE PAGE CONTAINING DOWNLOAD
     });
-    
+
   app.route('/api/igdownload/')
-    .get(async function (req, res, next){
+    .get(async function (req, res, next) {
       const mediaId = req.query.mediaId;
       // console.log(req.query);
-      
+
       searchDB(model, mediaId)
-      .then(url => {
-        const _stream = false
-        stream(url, _stream, res);
-      })
-      .catch((err) => {
-        console.error(err)
-        res.status(500).send({message: `Media not found. Please use Orangebot Instagram downloader first`})
-      })
+        .then(url => {
+          const _stream = false
+          stream(url, _stream, res);
+        })
+        .catch((err) => {
+          console.error(err)
+          res.status(500).send({ message: `Media not found. Please use Orangebot Instagram downloader first` })
+        })
     })
 
   app.route('/api/stream/')
-    .get(async function (req, res, next){
+    .get(async function (req, res, next) {
       const mediaId = req.query.mediaId;
       // console.log(req.query);
-      
+
       searchDB(model, mediaId)
-      .then(url => {
-        const _stream = true
-        stream(url, _stream, res);
-      })
-      .catch((err) => {
-        console.error(err)
-        res.status(500).send({message: `Media not found. Please use Orangebot Instagram downloader first`})
-      })
-    })    
+        .then(url => {
+          const _stream = true
+          stream(url, _stream, res);
+        })
+        .catch((err) => {
+          console.error(err)
+          res.status(500).send({ message: `Media not found. Please use Orangebot Instagram downloader first` })
+        })
+    })
 };
